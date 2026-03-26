2775d3f6fa9688edeb68f9e646e61fed3e06b8ce
SaviruWS <savirubabaranda2002@gmail.com>
2026-03-26 23:25:06 +0530

    Day28

--- controllers/userController.js
+++ controllers/userController.js
@@ -61,6 +61,7 @@ export function loginUser(req, res) {
 				);
 
 				if (isPasswordValid) {
+					//check if attempts are more that 3 times and if so, we do not send this token
 					const token = jwt.sign(
 						{
 							email: user.email,
@@ -70,22 +71,19 @@ export function loginUser(req, res) {
 							image: user.image,
 							isEmailVerified: user.isEmailVerified,
 						},
-						 process.env.JWT_SECRET,
-						 {expiresIn:req.body.rememberme ? "30d" : "48h"}
+						process.env.JWT_SECRET,
+						{ expiresIn: req.body.rememberme ? "30d" : "48h" }
 					);
-
-					
-
-					
 
 					res.json({
 						message: "Login successfull",
 						token: token,
-						role:user.role,
+						role: user.role,
 					});
 				} else {
 					res.status(401).json({
 						message: "Invalid password",
+						//we should add a record in data base of this failed attempt for the specific email
 					});
 				}
 			}
@@ -95,6 +93,77 @@ export function loginUser(req, res) {
 				message: "Internal server error",
 			});
 		});
+}
+
+export function getUser(req,res){
+
+	if(req.user == null){
+		res.status(401).json({
+			message: "Unauthorized"
+		})
+		return
+	}
+
+	res.json({
+		email: req.user.email,
+		firstName: req.user.firstName,
+		lastName: req.user.lastName,
+		role: req.user.role,
+		image: req.user.image,
+		isEmailVerified: req.user.isEmailVerified,
+	})
+
+
+}
+
+export async function updateUserProfile(req,res){
+
+	if(req.user == null){
+		res.status(401).json({
+			message: "Unauthorized"
+		})
+		return
+	}
+	try{
+		await User.updateOne({ email : req.user.email }, { firstName : req.body.firstName, lastName : req.body.lastName, image : req.body.image })
+		const user = await User.findOne({ email : req.user.email })
+		const token = jwt.sign(
+						{
+							email: user.email,
+							firstName: user.firstName,
+							lastName: user.lastName,
+							role: user.role,
+							image: user.image,
+							isEmailVerified: user.isEmailVerified,
+						},
+						process.env.JWT_SECRET,
+						{ expiresIn: req.body.rememberme ? "30d" : "48h" }
+					);
+		res.json({ message : "Profile updated successfully", token : token })
+	}catch(error){
+		res.status(500).json({ message : "Error updating profile", error : error })
+	}
+}
+
+export async function changeUserPassword(req,res){
+
+	if(req.user == null){
+		res.status(401).json({
+			message: "Unauthorized"
+		})
+		return
+	}
+
+	try{
+
+		const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+
+		await User.updateOne({ email : req.user.email }, { password : hashedPassword })
+		res.json({ message : "Password changed successfully" })
+
+	}catch(error){
+		res.status(500).json({ message : "Error changing password", error : error })
+	}
 }
 
 export function isAdmin(req){
